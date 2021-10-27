c85d1c37f84218a8de5933b1030af045cd41fb7c
Vladislav <vzv@siluetstudio.com>
2021-10-27 16:57:12 +0300

    almost full page

--- js/jquery-ready.js
+++ js/jquery-ready.js
@@ -39,16 +39,6 @@ $(document).ready(function() {
 
     //============Мобильное меню (КОНЕЦ)
 
-    //====Переключалка языков======
-    if ($(window).width() >= 992) {
-        $('.lang').click(function() {
-            $(this).find('.lang__toggle').toggleClass('active');
-            $('.lang__content').slideToggle();
-        })
-    }
-    
-    //====Переключалка языков КОНЕЦ======
-
     //=====Якорные ссылки====
     function anchorLinks () {
         let currentLink = $(this).attr('data-anchor');
@@ -70,161 +60,47 @@ $(document).ready(function() {
 
     //=====Якорные ссылки КОНЕЦ==
 
-    //=====табы в блоке УСТАНОВКА======
-    if ($('.install').length) {
-        $('.install .tabs').on('click', '.tabs__item:not(".active")', function() {
-           $(this).addClass('active').siblings().removeClass('active');
-
-           $(this).closest('.install').find('.install__item').eq($(this).index()).fadeIn().addClass('active').siblings('.install__item').hide().removeClass('active');
-
-
-           $('.install__item').find('.js-slider').slick('setPosition')
-
-        });
-    }
-    //=====табы в блоке УСТАНОВКА КОНЕЦ==============
-
-    //=====Слайдеры в блоке УСТАНОВКА======
-    if ($('.install .js-slider').length) {
-        $('.install .js-slider').slick({
+    //=====Слайдер======
+    if ($('.intro').length) {
+        $('.intro .slider').slick({
             slidesToShow: 1,
             dots: true,
+            appendDots: '.slider-pag',
             arrows: false
         })
     }
-    //=====Слайдеры в блоке УСТАНОВКА КОНЕЦ==============
+    //=====Слайдер КОНЕЦ==============
 
-    
-
-    //=====Слайдер Галерея=========
-    if($('.gallery').length) {
-        $('.js-gallery').slick({
-            dots: false,
-            slidesToShow: 1,
-            variableWidth: true,
-            infinite: false,
-            arrows: true,
-            responsive: [
-                {
-                    breakpoint: 993,
-                    settings: {
-                        variableWidth: false,
-                        slidesToShow: 2,
-                        infinite: true
-                    }
-                },
-                {
-                    breakpoint: 769,
-                    settings: {
-                        slidesToShow: 1,
-                        variableWidth: false,
-                        infinite: true,
-                        arrows: false
-                    }
-                }
-            ]
-        });
-
-        
-
-        $('.js-gallery').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-            let slideCount = $('.js-gallery .slick-slide').length;
-
-            if ($(window).width() >= 993) {
-                if (nextSlide == (slideCount - 1)) {
-                    $('.js-gallery .slick-next').addClass('last-slide')
-                } else {
-                    $('.js-gallery .slick-next').removeClass('last-slide')
-                }
-    
-                if (nextSlide == 0) {
-                    $('.js-gallery .slick-prev').removeClass('first-slide')
-                } else {
-                    $('.js-gallery .slick-prev').addClass('first-slide')
-                }
-            }
-            
-            
-        });
-
-        
-    }
-
-    //=====Слайдер Галерея КОНЕЦ========
-
-    
-    //=====Табы в свяжитесь с нами=====
-    if ($('.feedback').length) {
-        $('.feedback .tabs').on('click', '.tabs__item:not(".active")', function() {
-            $(this).addClass('active').siblings().removeClass('active');
-
-            $(this).closest('.install').find('.install__item').eq($(this).index()).fadeIn().addClass('active').siblings('.install__item').hide().removeClass('active');
-
-            
-            if ($(this).attr('data-tab') == "feedback_visit") {
-               $('.form__item[data-tab="feedback_message"]').hide().find('input, textarea').prop('disabled', true);
-               $('.form__item[data-tab="feedback_visit"]').show().find('input, textarea').prop('disabled', false);
-            } else if ($(this).attr('data-tab') == "feedback_message") {
-                $('.form__item[data-tab="feedback_visit"]').hide().find('input, textarea').prop('disabled', true);
-                $('.form__item[data-tab="feedback_message"]').show().find('input, textarea').prop('disabled', false);
-            }
-
-        });
-
-        $('.intro .btns').on('click', '.btns__item', function () {
-            let tab = $(this).attr('data-tab');
-
-            if (tab) {
-                $('.feedback .tabs .tabs__item[data-tab="'+ tab +'"]').addClass('active').siblings().removeClass('active');
-
-                if (tab == "feedback_visit") {
-                    $('.form__item[data-tab="feedback_message"]').hide().find('input, textarea').prop('disabled', true);
-                    $('.form__item[data-tab="feedback_visit"]').show().find('input, textarea').prop('disabled', false);
-                } else if (tab == "feedback_message") {
-                    $('.form__item[data-tab="feedback_visit"]').hide().find('input, textarea').prop('disabled', true);
-                    $('.form__item[data-tab="feedback_message"]').show().find('input, textarea').prop('disabled', false);
-                }
-            }
-            
+    //==========селекты в форме===========
+    if ($('.form__row_select').length) {
+        $('.form__item-select').each(function() {
+            $(this).select2({
+                placeholder: "Выбрать",
+            });
         })
     }
-    //=====Табы в свяжитесь с нами КОНЕЦ======
+    //=========== селекты в форме КОНЕЦ======
 
+    //======карта========
+    if($('.map').length) {
+        ymaps.ready(init);
+        function init(){
+            // Создание карты.
+            var myMap = new ymaps.Map("map", {                
+                center: [53.38891907106437,83.74414849999994],         
+                zoom: 16,
+                controls: []
+            });
 
-    //=====Показ имени файлов========
-    $('.form').on('change', 'input[type="file"]', function() {
-        console.log(1)
-        //$('.files-list__item').remove();
-        let fileName = $(this)[0].files[0].name;
-        let clone = $(this).closest('.form__file').clone();
-        
-        let fileElement = $(document.createElement('div'));
-        let fileElementDel = $(document.createElement('div'));
+            var myPlacemark = new ymaps.Placemark([53.38891907106437,83.74414849999994], {}, {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/icons/map-marker.png',
+                iconImageSize: [55, 57],
+                iconImageOffset: [-28, -45]
+            });
 
-        fileElement.addClass('files-list__item');
-        fileElementDel.addClass('delete');
-        fileElement.attr('data-count', $('.form__file').length)
-
-        fileElement.text(fileName);
-        $('.files-list').append(fileElement);
-        fileElement.append(fileElementDel);
-        
-        $('.form__item.file').append(clone)
-        $(this).closest('.form__file').hide();
-        clone.find('input').attr('data-count', $('.form__file').length);
-        clone.find('input').attr('name', 'file['+ $('.form__file').length +']');
-        
-    })
-
-    //удаление файлов
-
-    $('.form').on('click', '.delete', function() {
-        let fileItem = $(this).parent();
-        let fileItemCount = fileItem.attr('data-count');
-
-        $('input[data-count="'+ fileItemCount +'"]').val("");
-        fileItem.hide();        
-    })
-
-    //=====Показ имени файлов КОНЕЦ=======
+            myMap.geoObjects.add(myPlacemark);
+        }
+    }
+    //======карта КОНЕЦ======
 });
